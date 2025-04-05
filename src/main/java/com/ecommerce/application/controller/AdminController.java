b641a2b4b3ea74135952152182aa71bd65935d5f
abhinav-dwivedi-ttn <abhinav.dwivedi@tothenew.com>
2025-04-05 23:17:21 +0530

    Seller and Customer logout apis

--- src/main/java/com/ecommerce/application/controller/AdminController.java
+++ src/main/java/com/ecommerce/application/controller/AdminController.java
@@ -1,15 +1,15 @@
 package com.ecommerce.application.controller;
 
 import com.ecommerce.application.DTO.AdminLoginRequest;
-import com.ecommerce.application.DTO.SellerLoginRequest;
+import com.ecommerce.application.exception.CustomException;
+import com.ecommerce.application.exception.UnauthorizedException;
 import com.ecommerce.application.service.AdminService;
+import com.ecommerce.application.service.TokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -17,10 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminController {
 
     private final AdminService adminService;
+    private final TokenService tokenService;
 
     @PostMapping("/login")
     public ResponseEntity<String> loginAdmin(@Valid @RequestBody AdminLoginRequest request) {
         adminService.loginAdmin(request);
         return ResponseEntity.ok("Admin logged-in successfully!");
+    }
+
+    @PostMapping("/logout")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ResponseEntity<String> logoutSeller(@RequestHeader("Authorization") String request) {
+        if (request == null || !request.startsWith("Bearer ")) {
+            throw new CustomException("Access token is missing or invalid format!");
+        }
+
+        String token = request.substring(7);
+
+        if (!tokenService.isTokenValid(token)) {
+            throw new UnauthorizedException("Invalid or expired access token!");
+        }
+
+        tokenService.invalidateToken(token);
+
+        return ResponseEntity.ok("Logout successful. Access token is now invalidated.");
     }
 }
--- src/main/java/com/ecommerce/application/controller/SellerController.java
+++ src/main/java/com/ecommerce/application/controller/SellerController.java
@@ -2,10 +2,14 @@ package com.ecommerce.application.controller;
 
 import com.ecommerce.application.DTO.SellerLoginRequest;
 import com.ecommerce.application.DTO.SellerRegistrationRequest;
+import com.ecommerce.application.exception.CustomException;
+import com.ecommerce.application.exception.UnauthorizedException;
 import com.ecommerce.application.service.SellerService;
+import com.ecommerce.application.service.TokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class SellerController {
 
     private final SellerService sellerService;
+    private final TokenService tokenService;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerCustomer(@Valid @RequestBody SellerRegistrationRequest request) {
@@ -25,6 +30,24 @@ public class SellerController {
     public ResponseEntity<String> loginSeller(@Valid @RequestBody SellerLoginRequest request) {
         sellerService.loginSeller(request);
         return ResponseEntity.ok("Seller logged-in successfully!");
+    }
+
+    @PostMapping("/logout")
+    @PreAuthorize("hasAuthority('SELLER')")
+    public ResponseEntity<String> logoutSeller(@RequestHeader("Authorization") String request) {
+        if (request == null || !request.startsWith("Bearer ")) {
+            throw new CustomException("Access token is missing or invalid format!");
+        }
+
+        String token = request.substring(7);
+
+        if (!tokenService.isTokenValid(token)) {
+            throw new UnauthorizedException("Invalid or expired access token!");
+        }
+
+        tokenService.invalidateToken(token);
+
+        return ResponseEntity.ok("Logout successful. Access token is now invalidated.");
     }
 
 }
